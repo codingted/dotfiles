dc398c20f5df75f664f93aeea8c5de84369f407f
zhaoliang <zhaoliang12214@163.com>
2021-04-01 09:20:18 +0800

    modify

--- ranger/scope.sh
+++ ranger/scope.sh
@@ -53,7 +53,7 @@ handle_extension() {
             unrar lt -p- -- "${FILE_PATH}" && exit 5
             exit 1;;
         7z)
-            # Avoid password prompt by providing empty password
+            mg# Avoid password prompt by providing empty password
             7z l -p -- "${FILE_PATH}" && exit 5
             exit 1;;
 
